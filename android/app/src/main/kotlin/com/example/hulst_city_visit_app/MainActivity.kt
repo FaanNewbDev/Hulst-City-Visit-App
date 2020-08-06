7eee5bfb3ba14ac7cacf7ded758e185f57cba089
Stefaan D <sag.deconinck@gmail.com>
2020-08-06 20:48:36 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/hulst_city_visit_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hulst_city_visit_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Homepage(),
+      ),
+    );
+
+class Homepage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green[600],
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Icon(Icons.menu),
+        title: Text('Welcome & Explore Hulst'),
+        actions: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Container(
+              width: 30.0,
+              height: 30.0,
+              decoration: BoxDecoration(
+                color: Colors.green[800],
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Center(
+                child: Text('0'),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Container(),
+      ),
+    );
+  }
+}
