c6a5533dbcebc9e3ec3a24ff549c349240ba16df
Stefaan D <sag.deconinck@gmail.com>
2020-08-14 16:53:51 +0200

    2e try commit basic layout UI

--- lib/main.dart
+++ lib/main.dart
@@ -7,39 +7,118 @@ void main() => runApp(
     );
 
 class Homepage extends StatelessWidget {
+  final List<String> _listItem = [
+    'assets/images/ihulst_white_logo.png',
+    'assets/images/ihulst_white_logo.png',
+    'assets/images/ihulst_white_logo.png',
+    'assets/images/ihulst_white_logo.png',
+    'assets/images/ihulst_white_logo.png',
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.green[600],
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        leading: Icon(Icons.menu),
-        title: Text('Welcome & Explore Hulst'),
-        actions: <Widget>[
-          Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Container(
-              width: 30.0,
-              height: 30.0,
-              decoration: BoxDecoration(
-                color: Colors.green[800],
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Center(
-                child: Text('0'),
-              ),
-            ),
-          ),
-        ],
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          colors: [
+            Colors.green[50],
+            Colors.green[200],
+          ],
+        ),
+        //image: DecorationImage(
+        //  image: AssetImage('assets/images/2985315.jpg'),
+        //  fit: BoxFit.cover,
+        //),
       ),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          padding: EdgeInsets.all(20.0),
-          child: Column(
-            children: <Widget>[
-              Container()
-            ],
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          title: Text(
+            'Welcome & Explore',
+            style: TextStyle(color: Colors.grey[600]),
+          ),
+          leading: Icon(
+            Icons.menu,
+            color: Colors.grey[700],
+          ),
+          elevation: 0.0,
+        ),
+        body: SafeArea(
+          child: Container(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3),
+                      )
+                    ],
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/ihulst_white_logo.png'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  height: 250.0,
+                  width: double.infinity,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      SizedBox(height: 10.0),
+                      Text(
+                        'EVENTS',
+                        style: TextStyle(
+                            color: Colors.grey[600],
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Expanded(
+                  child: GridView.count(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 10.0,
+                    mainAxisSpacing: 10.0,
+                    children: _listItem
+                        .map(
+                          (item) => Card(
+                            elevation: 0,
+                            color: Colors.transparent,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey.withOpacity(0.5),
+                                    spreadRadius: 3,
+                                    blurRadius: 7,
+                                    offset: Offset(0, 3),
+                                  )
+                                ],
+                                image: DecorationImage(
+                                  image: AssetImage(item),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          ),
+                        )
+                        .toList(),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
